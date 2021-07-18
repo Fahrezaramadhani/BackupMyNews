6033a74a3cdb9d1a74008b99d4b541447c2ff5c3
Fahrezaramadhani <fahreza.ramadhani.tif19@polban.ac.id>
2021-07-18 16:55:36 +0700

    Second commit

--- app/src/main/java/com/example/mynews/NewsAPI.java
+++ app/src/main/java/com/example/mynews/NewsAPI.java
@@ -7,4 +7,14 @@ import retrofit2.http.Query;
 public interface NewsAPI {
     @GET("top-headlines")
     Call<NewsResponse> getNewsList(@Query("country") String newCountry, @Query("apiKey") String apiKey);
+    @GET("everything")
+    Call<NewsResponse> getNewsEverythingFilter(@Query("q") String katakunci, @Query("apiKey") String apiKey);
+    @GET("everything")
+    Call<NewsResponse> getNewsEverything(@Query("apiKey") String apiKey);
+    @GET("everything")
+    Call<NewsResponse> getNewsListFilter(@Query("q") String katakunci, @Query("apiKey") String apiKey);
+    @GET("sources")
+    Call<NewsResponse> getNewsSource(@Query("country") String country);
+//    @GET("everything")
+//    Call<NewsResponse>
 }
